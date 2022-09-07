53ae0e5f1d73b9664b003e9e48936bbb1c1f800e
TestVagrant <testvagrant@TestVagrants-MacBook-Pro.local>
2022-09-07 17:41:28 +0530

    initial commit

---
+++ src/main/java/APITesting/RESTAssurredTests.java
@@ -0,0 +1,12 @@
+package APITesting;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class RESTAssurredTests {
+    @Test
+    public void test(){
+        Assert.assertEquals(1,1);
+    }
+
+}
