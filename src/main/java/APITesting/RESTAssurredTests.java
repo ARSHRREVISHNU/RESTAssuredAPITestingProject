0709857ce977f9a24b413d9c1600acfe459c02b0
TestVagrant <testvagrant@TestVagrants-MacBook-Pro.local>
2022-09-08 12:33:56 +0530

    Post test added

--- src/main/java/APITesting/RESTAssurredTests.java
+++ src/main/java/APITesting/RESTAssurredTests.java
@@ -1,5 +1,6 @@
 package APITesting;
 
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -7,7 +8,7 @@ import static io.restassured.RestAssured.*;
 
 public class RESTAssurredTests {
     @Test
-    public void test(){
+    public void togGetAllUsers(){
 
         //given() - Arrange
         given()
@@ -21,4 +22,30 @@ public class RESTAssurredTests {
         Assert.assertEquals(1,1);
     }
 
+    @Test
+    public void toPostANewUser(){
+
+        //Arrange
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer 4cfc4a225c7c3756320db1a12fdb54df2f688250bc47287c2271dd4ddf701498")
+                .body(
+                        "{\n" +
+                                "  \"name\": \"Tenali Ramakrishna\",\n" +
+                                "  \"gender\": \"male\",\n" +
+                                "  \"email\": \"tenali.ramakrishna34@15ce.com\",\n" +
+                                "  \"status\": \"active\"\n" +
+                                "}"
+                )
+                //Act
+                .when()
+                .post("https://gorest.co.in/public/v2/users")
+        //Assert
+                .then()
+                .log().body()
+                .statusCode(201);
+
+
+    }
 }
