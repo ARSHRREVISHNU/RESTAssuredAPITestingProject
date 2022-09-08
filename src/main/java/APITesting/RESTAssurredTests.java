7ebe933ddc8377ac44518b14dc487f9032a9045b
TestVagrant <testvagrant@TestVagrants-MacBook-Pro.local>
2022-09-08 10:33:22 +0530

    Fist Api test

--- src/main/java/APITesting/RESTAssurredTests.java
+++ src/main/java/APITesting/RESTAssurredTests.java
@@ -3,10 +3,21 @@ package APITesting;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import static io.restassured.RestAssured.*;
+
 public class RESTAssurredTests {
     @Test
     public void test(){
 
+        //given() - Arrange
+        given()
+        //when() - Act
+                .when()
+                    .get("https://gorest.co.in/public/v2/users")
+        //then() - Assert
+                .then()
+                    .statusCode(200)
+                    .log().body();
         Assert.assertEquals(1,1);
     }
 
